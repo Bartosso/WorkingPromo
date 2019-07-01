820e256167945a9505370abaed916923c71df1eb
Bartosso <barton547@gmail.com>
2019-07-01 22:54:13 +0800

    Update Main.go

--- Main.go
+++ Main.go
@@ -15,7 +15,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	//По переходу на данный хендлер (т.е. localhost:8080/) запускается функция - обнови данные в бд по большим папкам
 	//типа там всякие игры, кредитки, мобильная связь и т.д.
-	//Теперь обновляет еще и игры с продовцами
+	//Теперь обновляет еще и игры с продавцами
 	models.UpdateBigFoldersAndGamesFromSections()
 	models.UpdateGamesOffersAndSellersStats()
 
